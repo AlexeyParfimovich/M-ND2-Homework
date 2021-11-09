694d265ab0d8ea28952a7f227480f21f5a19c5f2
AlexeyParfimovich <alexey.parfimovich@yandex.by>
2021-11-09 09:42:21 +0300

    lesson-second. implement getting and displaying a list of directories in a given directory

--- Lesson02/ConsoleApp1/Program.cs
+++ Lesson02/ConsoleApp1/Program.cs
@@ -1,12 +1,42 @@
 ﻿using System;
+using System.IO;
 
 namespace ConsoleApp1
 {
     class Program
     {
+        /// <summary>
+		/// App starting
+		/// </summary>
+		/// <param name="args">Command line args to start.</param>
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /*TODO: 
+             * Implement getting the path to the initial directory 
+             * and search and display parameters 
+             * through command line arguments
+             */
+
+            var path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+            Console.WriteLine($"List all subdirectories in {path}");
+
+            try 
+            { 
+                var diList = (new DirectoryInfo(path)).EnumerateDirectories("*", SearchOption.TopDirectoryOnly);
+
+                foreach (var di in diList)
+                {
+                    Console.WriteLine($"{di.FullName}");
+                }
+
+                Console.WriteLine("Press any key to exit...");
+                Console.ReadKey();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"{e.Message}");
+            }
         }
     }
 }
